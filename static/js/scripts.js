7bfb9438b71c80bc232861f640ee8a1720b9b988
alex <sania9991@yahoo.com>
2023-08-30 18:39:16 -0700

    Final Faders

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -31,6 +31,7 @@ accordions.forEach(accordion => {
 
 //Intersectional Observer
 
+/*
 const images = document.querySelectorAll('.about-tag');
 
 
@@ -52,6 +53,91 @@ observer = new IntersectionObserver((entries) => {
 images.forEach(image => {
 	observer.observe(image)
 })
+
+*/
+
+/*
+const sectionOne = document.querySelector('.about-tag');
+const sections = document.querySelectorAll('p');
+
+const options = {
+	root: null,
+	threshold: 0,
+	rootMargin: "-200px"
+ };
+
+const observer = new IntersectionObserver(function(entries, observer) {
+	entries.forEach(entry => {
+		if(!entry.isIntersecting) {
+			return;
+		}
+		console.log(entry.target);
+		entry.target.classList.toggle('inverse');
+		observer.unobserve(entry.target);
+	});
+}, options);
+
+sections.forEach(section => {
+	observer.observe(section);
+});
+
+*/
+
+const faders = document.querySelectorAll('.fade-in');
+const appearOptions = {
+	threshold: 1,
+	rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+	(function(
+		entries, 
+		appearOnScroll
+		) {
+		entries.forEach(entry => {
+			if (!entry.isIntersecting) {
+				return;
+			} else {
+				entry.target.classList.add('appear');
+				appearOnScroll.unobserve(entry.target);
+			}
+		});
+	}, 
+	appearOptions);
+
+faders.forEach(fader => {
+	appearOnScroll.observe(fader);
+});
+
+const typewriter = document.querySelectorAll('.anim1');
+const fadeOptions = {
+
+};
+
+const appearTypewriter = new IntersectionObserver
+	(function(
+		entries,
+		appearTypewriter
+		) {
+		entries.forEach(entry => {
+			if (!entry.isIntersecting) {
+				return;
+			} else {
+				entry.target.classList.add('fade');
+				appearTypewriter.unobserve(entry.target);
+			}
+		});
+	},
+	fadeOptions);
+
+	typewriter.forEach(typewriter => {
+		appearTypewriter.observe(typewriter);
+	});
+
+
+
+
+
 
 
 
